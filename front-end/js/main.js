01487b6fd25969316c8d241f268f4eafafc48d75
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-01-02 04:10:57 -0700

    game screen and keyboard input

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -2,4 +2,13 @@ const
     R = require('ramda'),
     Rx = require('rx')
 
-console.log(R, Rx)
+var c = document.getElementById("gameScreen");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();
+
+Rx.Observable.fromEvent(document, 'keydown')
+    .map(R.prop('key'))
+    .filter(R.pipe(R.match(/^ArrowUp|ArrowDown$/), R.length))
+    .subscribe(console.log)
--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,13 @@
 var R = require('ramda'),
     Rx = require('rx');
 
-console.log(R, Rx);
+var c = document.getElementById("gameScreen");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+
+Rx.Observable.fromEvent(document, 'keydown').map(R.prop('key')).filter(R.pipe(R.match(/^ArrowUp|ArrowDown$/), R.length)).subscribe(console.log);
 
 },{"ramda":2,"rx":311}],2:[function(require,module,exports){
 module.exports = {
