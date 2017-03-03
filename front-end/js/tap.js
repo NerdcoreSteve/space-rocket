4b9934cd320b74c8ae87716f0568eeb37987b6f6
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-03-03 14:04:30 -0700

    moving improved tap to master

--- front-end/js/tap.js
+++ front-end/js/tap.js
@@ -1,1 +1,11 @@
-module.exports = x => { console.log(x); return x }
+const
+    R = require('ramda')
+
+var
+    tap = x => { console.log(x); return x }
+
+tap.filter = R.curry((f, x) => { if(f(x)) console.log(x); return x })
+tap.lens = R.curry((l, x) => { console.log(l(x)); return x })
+tap.lensFilter = R.curry((l, f, x) => { if(f(x)) console.log(l(x)); return x })
+
+module.exports = tap
